1a0dda0d3813dc8d60629a703d6b13451c62e4bb
Rakhi Sharma <rakhish1994@gmail.com>
2018-08-19 16:22:12 +0530

    Currency converter

---
+++ Javascript/index.js
@@ -0,0 +1,46 @@
+//Use fixer api to retrieve the exchange rates
+var baseurl = 'http://data.fixer.io/api/';
+
+// get the fiels using class and id
+const inputVal = document.querySelector(".amount"); //user input
+const selectFrom = document.getElementById('fromCountry'); //get the current currency
+const selectTo = document.getElementById('toCountry'); //get the currency user want to see the exchange rate
+const resultAmount = document.querySelector(".result"); //show the conversted rate in choosed currency.
+
+inputVal.addEventListener("input", moneyConvert);
+
+function moneyConvert() {
+    //AJAX call via axios
+    axios.get(baseurl + 'latest?access_key=45dabd22444f06182c0708a187d63855')
+        .then(response => {
+            let data = response.data;
+            const euroAmount = data.rates.EUR; //latest EURO rate
+            const usdollar = data.rates.USD; //latest USD rate
+            const yen = data.rates.JPY; //latest JPY rate
+            const getAmount = inputVal.value; //amount that user entered
+
+            // create an object of all the rates.
+            let rates = {
+                USD: {
+                    USD: 1,
+                    Euro: (1 * euroAmount) / euroAmount,
+                    JPY: (1 * yen) / usdollar,
+                },
+                Euro: {
+                    Euro: 1,
+                    USD: (1 * usdollar) / euroAmount,
+                    JPY: (1 * yen) / euroAmount,
+                },
+                JPY: {
+                    JPY: 1,
+                    USD: (1 * usdollar) / yen,
+                    Euro: (1 * euroAmount) / yen,
+                }
+            }
+            // check + connvert rate
+            if (rates[selectFrom.value] && rates[selectFrom.value][selectTo.value]) {
+                resultAmount.value = getAmount * rates[selectFrom.value][selectTo.value];
+                console.log(resultAmount.value)
+            }
+        });
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # CurrencyConvertor
 Currency converter using Axios ajax call and fixerAPI
+
+#### Project is available online here:  
+
+##### https://rakhisharma.github.io/CurrencyConvertor/
